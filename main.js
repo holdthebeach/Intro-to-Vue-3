117506a2b6c2ff379f7ffca2a347a72e47ad30e2
Patty <patty@Patricias-MacBook-Pro.local>
2023-09-28 12:17:49 +0200

    created vue app in JS file and mounted it in index by referring to element with app id

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    // data: function() {
+    //     return {
+    //         product: 'Socks'
+    //     }
+    // }
+    //pre-ES6 syntax
+
+    data() {
+        return {
+            product: 'Socks'
+        }
+    }
+})
+//initiating the app by passing an object argument
+//with data as a property, whose value is a function is returning 
+//another object where we store our data
+
+

0c88aaed28cb6d85cc841590418239eaf8afec69
Patty <patty@Patricias-MacBook-Pro.local>
2023-09-28 12:49:07 +0200

    add url value to data options object and bind to new anchor tag that wraps around the image tag

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            description: 'royal green socks with vue mastery logo'
+            description: 'royal green socks with vue mastery logo',
+            url: 'https://www.img-hosting.com/royal-green-socks'
         }
     }
 })

866ac9173c331b3d46c56fb641bc97e4d195829e
Patty <patty@Patricias-MacBook-Pro.local>
2023-09-28 12:45:55 +0200

    add alt description and bind to alt attribute with v-bind shorthand

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            description: 'royal green socks with vue mastery logo'
         }
     }
 })
